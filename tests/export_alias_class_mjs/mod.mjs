39458c4873e15af9128a655fbf66144ec13e81e4
Abhijeet Prasad <abhijeet@braintrustdata.com>
2026-02-24 15:25:40 -0500

    remove comments

--- tests/export_alias_class_mjs/mod.mjs
+++ tests/export_alias_class_mjs/mod.mjs
@@ -1,7 +1,3 @@
-/**
- * Unless explicitly stated otherwise all files in this repository are licensed under the Apache-2.0 License.
- * This product includes software developed at Datadog (https://www.datadoghq.com/). Copyright 2025 Datadog, Inc.
- **/
 class J {
   async fetch(url) {
     return 42;
--- tests/export_alias_class_mjs/test.mjs
+++ tests/export_alias_class_mjs/test.mjs
@@ -1,7 +1,3 @@
-/**
- * Unless explicitly stated otherwise all files in this repository are licensed under the Apache-2.0 License.
- * This product includes software developed at Datadog (https://www.datadoghq.com/). Copyright 2025 Datadog, Inc.
- **/
 import { Undici } from './instrumented.mjs';
 import { assert, getContext } from '../common/preamble.js';
 const context = getContext('orchestrion:undici:Undici:fetch');
