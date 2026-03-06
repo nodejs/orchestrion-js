b17de5f83f312409262f2b8822b8a0be87e031d1
rochdev <roch.devost@gmail.com>
2026-03-06 12:35:36 -0500

    wip

--- index.ts
+++ index.ts
@@ -1,22 +1,11 @@
-import type { InstrumentationConfig, InstrumentationMatcher, create as internalCreate } from './pkg/orchestrion_js';
-
-// ./pkg/orchestrion_js.js has a side effect of loading the wasm binary. 
-// We only want that if the library is actually used!
-let cachedCreate: typeof internalCreate | undefined;
+import type { InstrumentationConfig, InstrumentationMatcher } from './types.d.ts';
+import { create as internalCreate } from './lib'
 
 /**
  * Create a new instrumentation matcher from an array of instrumentation configs.
  */
 export function create(configs: InstrumentationConfig[], dc_module?: string | null): InstrumentationMatcher {
-    if (!cachedCreate) {
-        cachedCreate = require('./pkg/orchestrion_js.js').create;
-    }
-
-    if (cachedCreate === undefined) {
-        throw new Error("Failed to load '@apm-js-collab/code-transformer'");
-    }
-
-    return cachedCreate(configs, dc_module);
+    return internalCreate(configs, dc_module);
 }
 
 export type {
@@ -28,4 +17,4 @@ export type {
     ModuleType,
     TransformOutput,
     Transformer,
-} from './pkg/orchestrion_js'
+} from './types.d.ts'
