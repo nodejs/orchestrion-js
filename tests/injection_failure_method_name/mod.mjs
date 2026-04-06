8b90658db8e3636b29cf24c3ce7302dec0e8df8d
Bob Evans <robert.evans25@gmail.com>
2026-04-06 16:37:15 -0400

    fix: Updated transformer to prevent instrumenting a non-existent method on a class

---
+++ tests/injection_failure_method_name/mod.mjs
@@ -0,0 +1,11 @@
+class Foo {
+  constructor () {
+    this.value = 42
+  }
+
+  doStuff () {
+    return true
+  }
+}
+
+export default Foo
---
+++ tests/injection_failure_method_name_subclass/mod.mjs
@@ -0,0 +1,9 @@
+class Base {}
+
+class Foo extends Base {
+  doStuff () {
+    return true
+  }
+}
+
+export default Foo
