c8781167f1b1433ee0f4708ebeb186c3c6af3642
Tim Fish <tim@timfish.uk>
2026-07-09 18:36:57 +0100

    feat: Remove unused free() methods

--- index.d.ts
+++ index.d.ts
@@ -86,7 +86,6 @@ export type ModuleType = "esm" | "cjs" | "unknown";
  */
 export class InstrumentationMatcher {
   private constructor();
-  free(): void;
   /**
    * Get a transformer for the given module name, version and file path.
    * Returns `undefined` if no matching instrumentations are found.
@@ -103,8 +102,6 @@ export class InstrumentationMatcher {
  */
 export class Transformer {
   private constructor();
-  free(): void;
-
   /**
    * The name of the module to transform.
    */
