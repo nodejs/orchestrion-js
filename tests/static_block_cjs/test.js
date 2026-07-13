e8760933ac16a0dfacacd7819811c5127243e41d
isaacs <i@izs.me>
2026-07-13 11:05:47 -0700

    fix: handle nameless blocks in class bodies
    This is needed to support `static {...}` blocks.

---
+++ tests/static_block_cjs/test.js
@@ -0,0 +1,21 @@
+/**
+ * Unless explicitly stated otherwise all files in this repository are licensed under the Apache-2.0 License.
+ * This product includes software developed at Datadog (https://www.datadoghq.com/). Copyright 2025 Datadog, Inc.
+ **/
+const Undici = require('./instrumented.js')
+const { assert, getContext } = require('../common/preamble.js')
+const context = getContext('orchestrion:undici:Undici:fetch');
+(async () => {
+  // Static init blocks are preserved and still run.
+  assert.strictEqual(Undici.KIND, 'undici')
+  assert.strictEqual(Undici.VERSION, 1)
+  const undici = new Undici()
+  const result = await undici.fetch('https://example.com')
+  assert.strictEqual(result, 42)
+  assert.deepStrictEqual(context, {
+    start: true,
+    end: true,
+    asyncStart: 42,
+    asyncEnd: 42
+  })
+})()
